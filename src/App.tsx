e361dc58e94b963960cceb004d1ea03bbd4c228f
tiptopassets <tiptopassets@gmail.com>
2026-04-09 18:16:07 -0400

    feat: same as last something broke and now all is blue only fix whats broken n

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 // @ts-nocheck
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import PlanearViajePage from './pages/PlanearViajePage';
-import PaquetesPage from './pages/PaquetesPage';
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import PlanearViajePage from './pages/PlanearViajePage'
+import PaquetesPage from './pages/PaquetesPage'
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-navy-950 font-sans selection:bg-blue-500/30">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/planear" element={<PlanearViajePage />} />
-          <Route path="/paquetes" element={<PaquetesPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+    <div className="min-h-screen bg-navy-900 font-sans selection:bg-blue-500/30">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/planear" element={<PlanearViajePage />} />
+        <Route path="/paquetes" element={<PaquetesPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
 }
-
-export default App;
